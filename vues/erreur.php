d6b342ed456ac69fed63204647cf93bd20270fc4
Sebastien-WAGNER-63 <sebastien.wagner.6320@gmail.com>
2020-12-15 15:44:21 +0100

    css

--- vues/erreur.php
+++ vues/erreur.php
@@ -2,15 +2,15 @@
     <head>
         <meta charset="utf-8">
         <title>Erreur</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" type="text/css" />
-        <link rel="stylesheet" href="css/styleErreur.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="vues/css/bootstrap.min.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="vues/css/styleErreur.css" media="screen" type="text/css" />
     </head>
     <body>
-         <div class="container-fluid ban">
+        <div class="container-fluid ban">
             <div class="row">
                 <article class="col-md-1">
-                    <a href="connexion.html">
-                        <img src="img/logo.png" class="img">
+                    <a href="index.php">
+                        <img src="vues/img/logo.png" class="img">
                     </a>
                 </article>
             </div>
@@ -19,18 +19,21 @@
                 
         </div>
         <div class="container-fluid">
-            <div class="container">
+            <div class="container blanco">
                 <div class="row">
-                    
+                    <article class="col-md-12">
+                        <div class="cont"><h1>Oups!</h1></div>
+                        <div>
+                            <?php
+                                foreach($TErreur as $val){
+                                    echo("<p>$val</p>");
+                                }
+                            ?>
+                        </div>
+                        <div class="cont"><button onclick="window.location.href = 'index.php';">Retour au site</button></div>
+                    </article>
                 </div>
             </div>
         </div>
     </body>
 </html>
-
-<?php
-    echo("<br/><b>Erreurs : </b><br/>");
-    foreach($TErreur as $val){
-        echo("⚠️ $val<br/>");
-    }
-?>
